eb612d2ed81142f8630395ef78bfaff4e6f01c3e
chae1412 <jeoncr8381@gmail.com>
2024-02-22 11:40:43 +0900

    first

---
+++ bbs/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from bbs import views
+
+urlpatterns = [
+    path('', views.BbsList.as_view()),
+    path('<int:pk>', views.BbsDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
